973d5d3738fdc573c189c5509a022fe8401c9d98
Junna <chuaijunna@bmbee.cn>
2015-12-07 20:53:58 +0800

    Junna: 增加LocalNotification demo

---
+++ LocalNotificationDemo/LocalNotificationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotificationDemo
+//
+//  Created by Junna on 12/7/15.
+//  Copyright © 2015 Junna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
